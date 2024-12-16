010e164f743eebca928d56eaa55cbaabfb84a9eb
Yingting Huang <huangyingting@outlook.com>
2024-12-16 18:57:36 +0800

    Update code

--- deployment/rg.tf
+++ deployment/rg.tf
@@ -1,5 +1,5 @@
 # Create a resource group
 resource "azurerm_resource_group" "wlid" {
-  name     = "${var.prefix}-rg"
+  name     = "${var.prefix}-${var.environment}"
   location = var.location
 }
--- deployment/variables.tf
+++ deployment/variables.tf
@@ -1,7 +1,14 @@
 variable "prefix" {
+  type = string
   default = "wlid"
 }
 
 variable "location" {
+  type = string
   default = "southeastasia"
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
 }
